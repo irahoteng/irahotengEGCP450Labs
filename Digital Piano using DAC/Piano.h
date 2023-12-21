20bf25b7525f97b11d86dc265ff56aa97c2ded59
Irah Nii-Kwame Oteng - Quarshie <60246472+irahoteng@users.noreply.github.com>
2023-12-21 11:20:22 -0800

    Create Piano.h

---
+++ Digital Piano using DAC/Piano.h
@@ -0,0 +1,16 @@
+/*
+ * Piano.h
+ *
+ *  Created on: Oct 23, 2023
+ *      Author: niidr
+ */
+
+#ifndef PIANO_H_
+#define PIANO_H_
+void Piano_Init(void); // We will be initializing Port 5 GPIOs
+char Piano_In(void);//Piano inputs
+
+
+
+
+#endif /* PIANO_H_ */
