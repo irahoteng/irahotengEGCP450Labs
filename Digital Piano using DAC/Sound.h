d3c12cb4b7440f9d2bf80b33b66084166e6495fe
Irah Nii-Kwame Oteng - Quarshie <60246472+irahoteng@users.noreply.github.com>
2023-12-21 11:22:10 -0800

    Create Sound.h

---
+++ Digital Piano using DAC/Sound.h
@@ -0,0 +1,17 @@
+/*
+ * Sound.h
+ *
+ *  Created on: Oct 23, 2023
+ *      Author: niidr
+ */
+
+#ifndef SOUND_H_
+#define SOUND_H_
+
+void Sound_Init(void);
+void Sound_Music(void);
+void Sound_Off(void);
+
+
+
+#endif /* SOUND_H_ */
